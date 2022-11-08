3e8034791aa3a81e26680bb6521ab6e951657262
danieletouke <108566435+danieletouke@users.noreply.github.com>
2022-11-08 03:53:06 -0500

    initial commit

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket         = "codixtech-state-file-1984"
+    key            = "codixtech/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
